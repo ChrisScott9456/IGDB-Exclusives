1fcefcbe7bd216e9d76fb919a8c47ec71c313966
ChrisScott9456 <christopherscott9456@gmail.com>
2022-09-15 23:08:43 -0400

    Set up basic authentication and test first call to IGDB APIs

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,46 @@
-console.log('Hello World!');
+import axios, { AxiosRequestConfig } from 'axios';
+import to from 'await-to-js';
+
+const fs = require('fs');
+const keyfile = require('../keyfile.json');
+
+async function authenticate() {
+	const [e, r] = await to(axios.post(`https://id.twitch.tv/oauth2/token?client_id=${keyfile.client_id}&client_secret=${keyfile.client_secret}&grant_type=client_credentials`));
+
+	if (e) {
+		throw e;
+	} else if (r) {
+		keyfile.token = r.data.access_token;
+
+		await fs.writeFileSync('keyfile.json', `{ "client_id": "${keyfile.client_id}", "client_secret": "${keyfile.client_secret}", "token": "${r.data.access_token}" }`, (err: Error) => {
+			if (err) {
+				console.error(err);
+			}
+		});
+	}
+
+	return {
+		'Client-ID': keyfile.client_id,
+		Authorization: `Bearer ${keyfile.token}`,
+	};
+}
+
+(async () => {
+	try {
+		const config: AxiosRequestConfig = {
+			headers: await authenticate(),
+		};
+
+		const [e, r] = await to(axios.post('https://api.igdb.com/v4/games', 'fields name,platforms;', config));
+
+		if (e) {
+			throw e;
+		}
+
+		console.log(r);
+		console.log();
+	} catch (e) {
+		console.error(e);
+		console.log();
+	}
+})();
